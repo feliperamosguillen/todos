454a9d8aa3b5ad6f42a1ed8a5f1dc559c50a20ea
Felipe Ramos <night76@hotmail.com>
2019-04-20 15:50:07 -0500

    Added layout file to views

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Todo item</title>
+@extends('layouts.app')
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-
-    <div class="container">
+@section('content')
         <h1 class="text-center my-5">{{ $todo->name }}</h1>
 
         <div class="row justify-content-center">
@@ -25,7 +15,4 @@
                 </div>
             </div>
         </div>
-    </div>
-
-</body>
-</html>
+@endsection
